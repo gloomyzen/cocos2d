d9006ff367edeea4923e6455d7b706e27e1d2a99
Artur Barilo <faustkeller45@gmail.com>
2021-03-12 23:56:09 +0900

    (feature)master | vs fixes

--- cocos/cocos2d.h
+++ cocos/cocos2d.h
@@ -280,6 +280,6 @@ NS_CC_END
 
 // Strings
 
-#define STRING_FORMAT(string, ...) cocos2d::StringUtils::format(string, __VA_ARGS__)
+#define STRING_FORMAT(string, ...) cocos2d::StringUtils::format(string, ##__VA_ARGS__)
 
 #endif // __COCOS2D_H__
